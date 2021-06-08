c3397f40e8a00552772b2a7c8e1e1c48cc969a26
fernando.js8 <fernando.js8@outlook.com>
2021-06-08 02:48:42 -0300

    LadmarkAI Done

---
+++ LandmarkAI/LandmarkAI/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using LandmarkAI.Classes;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace LandmarkAI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void selectImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png *.jpg)|*.png;*.jpg;*.jpeg;|All Files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+
+                await MakePredictionAsync(fileName);
+                selectedImage.Source = new BitmapImage(new Uri(fileName));
+            }
+        }
+
+        private async Task MakePredictionAsync(string fileName)
+        {
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/f74569a9-2447-4f34-83d5-ce9b500a9098/classify/iterations/Iteration1/image";
+            string prediction_key = "dab496c9368c4cb695bb5df9bc18b56a";
+            string content_type = "application/octet-stream";
+
+            var file = File.ReadAllBytes(fileName);
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("Prediction-Key", prediction_key);
+
+                using (var content = new ByteArrayContent(file))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue(content_type);
+                    var response = await httpClient.PostAsync(url, content);
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    List<Prediction> predictions = JsonConvert.DeserializeObject<CustomVision>(responseString).Predictions;
+
+                    resultsListView.ItemsSource = predictions;
+                }
+            }
+        }
+
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+              { Endpoint = endpoint };
+            return client;
+        }
+    }
+}
