3b1de8d4233d15dec0ca3946c32cc4de26a50c8d
fernando.js8 <fernando.js8@outlook.com>
2021-06-09 00:07:22 -0300

    Searchcommand Done

---
+++ WeatherApp/WeatherApp/ViewModel/Commands/SearchCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WeatherApp.ViewModel.Commands
+{
+    public class SearchCommand : ICommand
+    {
+        public WeatherVM _vm { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public SearchCommand(WeatherVM vm)
+        {
+            _vm = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _vm.MakeQuery();
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+using WeatherApp.Model;
+using WeatherApp.ViewModel.Commands;
+using WeatherApp.ViewModel.Helpers;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        public WeatherVM()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                selectedCity = new City()
+                {
+                    LocalizedName = "New York"
+                };
+
+                currentConditions = new CurrentConditions()
+                {
+                    WeatherText = "Sunny Day",
+                    Temperature = new Temperature()
+                    {
+                        Metric = new Units()
+                        {
+                            Value = 21
+                        }
+                    }
+                };
+            }
+
+            SearchCommand = new SearchCommand(this);
+        }
+
+        public SearchCommand SearchCommand { get; set; }
+
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set { query = value; OnPropertyChanged("Query"); }
+        }
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set { currentConditions = value; OnPropertyChanged("CurrentConditions"); }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set { selectedCity = value; OnPropertyChanged("SelectedCity"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async void MakeQuery()
+        {
+            var cities = await AccuWeatherHelper.GetCities(Query);
+        }
+    }
+}
