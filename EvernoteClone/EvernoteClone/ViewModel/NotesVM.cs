906eb5ddd544f8293a2c1fc181f227f25dccdd73
fernando.js8 <fernando.js8@outlook.com>
2021-06-13 12:05:14 -0300

    UI and BackEnd basic interaction

--- EvernoteClone/EvernoteClone/ViewModel/NotesVM.cs
+++ EvernoteClone/EvernoteClone/ViewModel/NotesVM.cs
@@ -3,33 +3,53 @@ using EvernoteClone.ViewModel.Commands;
 using EvernoteClone.ViewModel.Helpers;
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 
 namespace EvernoteClone.ViewModel
 {
-    public class NotesVM
+    public class NotesVM : INotifyPropertyChanged
     {
-        public ObservableCollection<Notebook> Notebooks { get; set; }
-
         private Notebook selectedNotebook;
+
 
         public Notebook SelectedNotebook
         {
             get { return selectedNotebook; }
             set
             {
-                selectedNotebook = value;
-                //TODO: get notes
+                if (value != null)
+                {
+                    selectedNotebook = value;
+
+                    OnPropertyChanged("SelectedNotebook");
+                    GetNotes();
+                }
             }
         }
 
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ObservableCollection<Notebook> Notebooks { get; set; }
         public ObservableCollection<Note> Notes { get; set; }
         public NewNotebookCommand NewNoteBookCommand { get; set; }
         public NewNoteCommand NewNoteCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public NotesVM()
         {
             NewNoteBookCommand = new NewNotebookCommand(this);
             NewNoteCommand = new NewNoteCommand(this);
+
+            Notebooks = new ObservableCollection<Notebook>();
+            Notes = new ObservableCollection<Note>();
+
+            GetNotebooks();
+            GetNotes();
         }
 
         public void CreateNotebook()
@@ -40,6 +60,8 @@ namespace EvernoteClone.ViewModel
             };
 
             DatabaseHelper.Insert(noteBook);
+
+            GetNotebooks();
         }
 
         public void CreateNote(int notebookId)
@@ -55,6 +77,31 @@ namespace EvernoteClone.ViewModel
                 };
 
                 DatabaseHelper.Insert(note);
+
+                GetNotes();
+            }
+        }
+
+        private void GetNotebooks()
+        {
+            Notebooks.Clear();
+
+            var notebooks = DatabaseHelper.Read<Notebook>();
+
+            foreach (var notebook in notebooks)
+                Notebooks.Add(notebook);
+        }
+
+        private void GetNotes()
+        {
+            if (SelectedNotebook != null)
+            {
+                Notes.Clear();
+
+                var notes = DatabaseHelper.Read<Note>().Where(n => n.NotebookId.Equals(SelectedNotebook.Id)).ToList();
+
+                foreach (var note in notes)
+                    Notes.Add(note);
             }
         }
     }
